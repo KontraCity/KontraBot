95c127b236a09b7ec7c20eea637ff5e5005d450a
KontraCity <nikita2006pro@gmail.com>
2024-02-22 13:32:56 +0300

    Bot embeds implementation

---
+++ include/bot/embeds.hpp
@@ -0,0 +1,155 @@
+﻿#pragma once
+
+// STL modules
+#include <optional>
+
+// Library DPP
+#include <dpp/dpp.h>
+
+// Custom modules
+#include "bot/signal.hpp"
+#include "bot/locale/locale.hpp"
+#include "common/utility.hpp"
+#include "youtube/item.hpp"
+#include "youtube/search.hpp"
+
+namespace kc {
+
+namespace Bot
+{
+    namespace Embeds
+    {
+        namespace Statuses
+        {
+            namespace Success
+            {
+                constexpr uint32_t Color = dpp::colors::dark_green;
+                constexpr const char* Prefix = u8"✅ ";
+            }
+
+            namespace Problem
+            {
+                constexpr uint32_t Color = dpp::colors::red;
+                constexpr const char* Prefix = u8"❌ ";
+            }
+
+            namespace Question
+            {
+                constexpr uint32_t Color = dpp::colors::yellow;
+                constexpr const char* Prefix = u8"❔ ";
+            }
+        }
+
+        /// @brief Create "Joined voice channel" embed
+        /// @param locale Locale to create embed for
+        /// @param channelId ID of voice channel
+        /// @return Created embed in a message
+        dpp::message Joining(const Locale::Pointer& locale, dpp::snowflake channelId);
+
+        /// @brief Create "Already joined voice channel" embed
+        /// @param locale Locale to create embed for
+        /// @param channelId ID of voice channel
+        /// @return Created embed in a message
+        dpp::message AlreadyJoined(const Locale::Pointer& locale, dpp::snowflake channelId);
+
+        /// @brief Create "You are not in voice channel to join" embed
+        /// @param locale Locale to create embed for
+        /// @return Created embed in an ephemeral message
+        dpp::message CantJoin(const Locale::Pointer& locale);
+
+        /// @brief Create "Left voice channel" embed
+        /// @param locale Locale to create embed for
+        /// @param channelId ID of voice channel
+        /// @return Created embed in a message
+        dpp::message Left(const Locale::Pointer& locale, dpp::snowflake channelId);
+
+        /// @brief Create "Not sitting in voice channel to leave" embed
+        /// @param locale Locale to create embed for
+        /// @return Created embed in an ephemeral message
+        dpp::message CantLeave(const Locale::Pointer& locale);
+
+        /// @brief Create "Do you want to play whole playlist or just this video?" embed
+        /// @param locale Locale to create embed for
+        /// @param videoId Video ID
+        /// @param playlistId Playlist ID
+        /// @return Created embed in an ephemeral message with buttons
+        dpp::message AmbigousPlay(const Locale::Pointer& locale, const std::string& videoId, const std::string& playlistId);
+
+        /// @brief Create "Item added to queue" embed
+        /// @param locale Locale to create embed for
+        /// @param item Added item
+        /// @return Created embed in a message
+        dpp::message ItemAdded(const Locale::Pointer& locale, const Youtube::Item& item, const std::optional<dpp::user>& requester = {});
+
+        /// @brief Create "I can't play livestreams" embed
+        /// @param locale Locale to create embed for
+        /// @param livestream Livestream in question
+        /// @param requester Requester user info to be shown in embed if needed
+        /// @return Created embed in an ephemeral message
+        dpp::message LivestreamsCantBePlayed(const Locale::Pointer& locale, const Youtube::Video& livestream, const std::optional<dpp::user>& requester = {});
+
+        /// @brief Create "I can't play upcoming videos" embed
+        /// @param locale Locale to create embed for
+        /// @param upcomingVideo Upcoming video in question
+        /// /// @param requester Requester user info to be shown in embed if needed
+        /// @return Created embed in an ephemeral message
+        dpp::message UpcomingVideosCantBePlayed(const Locale::Pointer& locale, const Youtube::Video& upcomingVideo, const std::optional<dpp::user>& requester = {});
+
+        /// @brief Create "Couldn't add item because of YouTube error" embed
+        /// @param locale Locale to create embed for
+        /// @param error Error in question
+        /// @return Created embed in an ephemeral message
+        dpp::message CouldntAddYoutubeError(const Locale::Pointer& locale, const Youtube::YoutubeError& error);
+
+        /// @brief Create "Couldn't add item because of local error" embed
+        /// @param locale Locale to create embed for
+        /// @param error Error in question
+        /// @return Created embed in an ephemeral message
+        dpp::message CouldntAddLocalError(const Locale::Pointer& locale, const Youtube::LocalError& error);
+
+        /// @brief Create "Couldn't add item because of unknown error" embed
+        /// @param locale Locale to create embed for
+        /// @return Created embed in an ephemeral message
+        dpp::message CouldntAddUnknownError(const Locale::Pointer& locale);
+
+        /// @brief Create search result embed
+        /// @param locale Locale to create embed for
+        /// @param searchResult Search result
+        /// @return Created embed in an ephemeral message with select menu
+        dpp::message Search(const Locale::Pointer& locale, const Youtube::SearchResult& searchResult);
+
+        /// @brief Create bad seek embed
+        /// @param locale Locale to create embed for
+        /// @param timestampString Seek timestamp string
+        /// @return Created embed in an ephemeral message
+        dpp::message BadSeek(const Locale::Pointer& locale, const std::string& timestampString);
+
+        /// @brief Create "Not playing to seek" embed
+        /// @param locale Locale to create embed for
+        /// @return Created embed in an ephemeral message
+        dpp::message NotPlayingToSeek(const Locale::Pointer& locale);
+
+        /// @brief Create "Seeking to timestamp" embed
+        /// @param locale Locale to create embed for
+        /// @param timestamp Timestamp seeking to
+        /// @return Created embed in a message
+        dpp::message SeekingTo(const Locale::Pointer& locale, const std::string& timestamp);
+
+        /// @brief Create "I don't recognise this slashcommand" embed
+        /// @param locale Locale to create embed for
+        /// @return Created embed in an ephemeral message
+        dpp::message UnknownSlashcommand(const Locale::Pointer& locale);
+
+        /// @brief Create "I don't recognise this button" embed
+        /// @param locale Locale to create embed for
+        /// @return Created embed in an ephemeral message
+        dpp::message UnknownButton(const Locale::Pointer& locale);
+
+        /// @brief Create "I don't recognise this option" embed
+        /// @param locale Locale to create embed for
+        /// @return Created embed in an ephemeral message
+        dpp::message UnknownOption(const Locale::Pointer& locale);
+    }
+}
+
+} // namespace kc
---
+++ source/bot/embeds.cpp
@@ -0,0 +1,284 @@
+﻿#include "bot/embeds.hpp"
+
+namespace kc {
+
+static void CheckRequester(const Bot::Locale::Pointer& locale, dpp::embed& embed, const std::optional<dpp::user>& requester)
+{
+    if (!requester)
+        return;
+    embed.set_author(locale->UsedCommand(requester->format_username(), "play"), "", requester->get_avatar_url());
+}
+
+dpp::message Bot::Embeds::Joining(const Locale::Pointer& locale, dpp::snowflake channelId)
+{
+    dpp::embed embed;
+    embed.color = Statuses::Success::Color;
+    embed.description = Statuses::Success::Prefix + locale->Joining(channelId);
+    return dpp::message().add_embed(embed);
+}
+
+dpp::message Bot::Embeds::AlreadyJoined(const Locale::Pointer& locale, dpp::snowflake channelId)
+{
+    dpp::embed embed;
+    embed.color = Statuses::Success::Color;
+    embed.description = Statuses::Success::Prefix + locale->AlreadyJoined(channelId);
+    return dpp::message().add_embed(embed);
+}
+
+dpp::message Bot::Embeds::CantJoin(const Locale::Pointer& locale)
+{
+    dpp::embed embed;
+    embed.color = Statuses::Problem::Color;
+    embed.description = Statuses::Problem::Prefix + std::string(locale->CantJoin());
+    return dpp::message().add_embed(embed).set_flags(dpp::m_ephemeral);
+}
+
+dpp::message Bot::Embeds::Left(const Locale::Pointer& locale, dpp::snowflake channelId)
+{
+    dpp::embed embed;
+    embed.color = Statuses::Success::Color;
+    embed.description = Statuses::Success::Prefix + locale->Left(channelId);
+    return dpp::message().add_embed(embed);
+}
+
+dpp::message Bot::Embeds::CantLeave(const Locale::Pointer& locale)
+{
+    dpp::embed embed;
+    embed.color = Statuses::Problem::Color;
+    embed.description = Statuses::Problem::Prefix + std::string(locale->CantLeave());
+    return dpp::message().add_embed(embed).set_flags(dpp::m_ephemeral);
+}
+
+dpp::message Bot::Embeds::AmbigousPlay(const Locale::Pointer& locale, const std::string& videoId, const std::string& playlistId)
+{
+    dpp::embed embed;
+    embed.color = Statuses::Question::Color;
+    embed.description = Statuses::Question::Prefix + std::string(locale->AmbigousPlay());
+
+    dpp::component playVideoButton;
+    playVideoButton.type = dpp::cot_button;
+    playVideoButton.style = dpp::cos_danger;
+    playVideoButton.label = locale->PlayVideo();
+    playVideoButton.set_emoji(u8"▶️");
+    playVideoButton.set_id(Signal(Signal::Type::PlayVideo, videoId));
+
+    dpp::component playPlaylistButton;
+    playPlaylistButton.type = dpp::cot_button;
+    playPlaylistButton.style = dpp::cos_danger;
+    playPlaylistButton.label = locale->PlayPlaylist();
+    playPlaylistButton.set_emoji(u8"🔢");
+    playPlaylistButton.set_id(Signal(Signal::Type::PlayPlaylist, playlistId));
+
+    dpp::component buttonComponent;
+    buttonComponent.add_component(playVideoButton);
+    buttonComponent.add_component(playPlaylistButton);
+    return dpp::message().add_embed(embed).add_component(buttonComponent).set_flags(dpp::m_ephemeral);
+}
+
+dpp::message Bot::Embeds::ItemAdded(const Locale::Pointer& locale, const Youtube::Item& item, const std::optional<dpp::user>& requester)
+{
+    dpp::embed embed;
+    embed.color = Statuses::Success::Color;
+    CheckRequester(locale, embed, requester);
+    switch (item.type())
+    {
+        case Youtube::Item::Type::Video:
+        {
+            const Youtube::Video& video = std::get<Youtube::Video>(item);
+            embed.description = Statuses::Success::Prefix + fmt::format("[{}]({})", locale->ItemAdded(), video.watchUrl());
+            embed.url = video.watchUrl();
+            embed.add_field(video.title(), "");
+            embed.thumbnail = { video.thumbnailUrl() };
+            break;
+        }
+        case Youtube::Item::Type::Playlist:
+        {
+            const Youtube::Playlist& playlist = std::get<Youtube::Playlist>(item);
+            embed.description = Statuses::Success::Prefix + fmt::format("[{}]({})", locale->ItemAdded(), playlist.viewUrl());
+            embed.url = playlist.viewUrl();
+            embed.add_field(playlist.title(), "");
+            embed.thumbnail = { playlist.thumbnailUrl() };
+            break;
+        }
+    }
+    return dpp::message().add_embed(embed);
+}
+
+dpp::message Bot::Embeds::LivestreamsCantBePlayed(const Locale::Pointer& locale, const Youtube::Video& livestream, const std::optional<dpp::user>& requester)
+{
+    dpp::embed embed;
+    embed.color = Statuses::Problem::Color;
+    CheckRequester(locale, embed, requester);
+    embed.description = Statuses::Problem::Prefix + locale->CouldntAdd(livestream.title());
+    embed.description += fmt::format("\n```{}```\n", locale->LivestreamsCantBePlayed());
+    embed.thumbnail = { livestream.thumbnailUrl() };
+    return dpp::message().add_embed(embed).set_flags(dpp::m_ephemeral);
+}
+
+dpp::message Bot::Embeds::UpcomingVideosCantBePlayed(const Locale::Pointer& locale, const Youtube::Video& upcomingVideo, const std::optional<dpp::user>& requester)
+{
+    dpp::embed embed;
+    embed.color = Statuses::Problem::Color;
+    CheckRequester(locale, embed, requester);
+    embed.description = Statuses::Problem::Prefix + locale->CouldntAdd(upcomingVideo.title());
+    embed.description += fmt::format("\n```{}```\n", locale->UpcomingVideosCantBePlayed());
+    embed.thumbnail = { upcomingVideo.thumbnailUrl() };
+    return dpp::message().add_embed(embed).set_flags(dpp::m_ephemeral);
+}
+
+dpp::message Bot::Embeds::CouldntAddYoutubeError(const Locale::Pointer& locale, const Youtube::YoutubeError& error)
+{
+    dpp::embed embed;
+    embed.color = Statuses::Problem::Color;
+    embed.description = Statuses::Problem::Prefix + locale->CouldntAdd();
+    switch (error.type())
+    {
+        case Youtube::YoutubeError::Type::LoginRequired:
+            embed.description += fmt::format("```{}```", locale->YoutubePrivateVideo());
+            break;
+        case Youtube::YoutubeError::Type::Unplayable:
+            embed.description += fmt::format("```{}```", locale->YoutubeBlockedVideo());
+            break;
+        case Youtube::YoutubeError::Type::YoutubeError:
+            embed.description += fmt::format("```{}```", locale->YoutubeInternalError());
+            break;
+        case Youtube::YoutubeError::Type::PlaylistError:
+            embed.description += fmt::format("```{}```", locale->YoutubePrivatePlaylist());
+            break;
+        default:
+            embed.description += fmt::format("```{}```", locale->YoutubeUnknownError());
+            break;
+    }
+    return dpp::message().add_embed(embed).set_flags(dpp::m_ephemeral);
+}
+
+dpp::message Bot::Embeds::CouldntAddLocalError(const Locale::Pointer& locale, const Youtube::LocalError& error)
+{
+    dpp::embed embed;
+    embed.color = Statuses::Problem::Color;
+    embed.description = Statuses::Problem::Prefix + locale->CouldntAdd();
+    switch (error.type())
+    {
+        case Youtube::LocalError::Type::PlaylistOfUnplayableVideos:
+            embed.description += fmt::format("```{}```", locale->PlaylistOfUnplayableVideos());
+            break;
+        case Youtube::LocalError::Type::ShortsPlaylist:
+            embed.description += fmt::format("```{}```", locale->ShortsPlaylistsCantBePlayed());
+            break;
+        case Youtube::LocalError::Type::EmptyPlaylist:
+            embed.description += fmt::format("```{}```", locale->EmptyPlaylist());
+            break;
+    }
+    return dpp::message().add_embed(embed).set_flags(dpp::m_ephemeral);
+}
+
+dpp::message Bot::Embeds::CouldntAddUnknownError(const Locale::Pointer& locale)
+{
+    dpp::embed embed;
+    embed.color = Statuses::Problem::Color;
+    embed.description = Statuses::Problem::Prefix + locale->CouldntAdd();
+    embed.description += fmt::format("```{}```", locale->SomethingWentWrong());
+    return dpp::message().add_embed(embed).set_flags(dpp::m_ephemeral);
+}
+
+dpp::message Bot::Embeds::Search(const Locale::Pointer& locale, const Youtube::SearchResult& searchResult)
+{
+    if (searchResult.items.size() == 0)
+    {
+        dpp::embed embed;
+        embed.color = Statuses::Problem::Color;
+        embed.description = Statuses::Problem::Prefix + locale->SearchResult(0);
+        return dpp::message().add_embed(embed).set_flags(dpp::m_ephemeral);
+    }
+
+    dpp::embed embed;
+    embed.color = Statuses::Success::Color;
+    embed.description = Statuses::Success::Prefix + std::string(locale->WhatAreWePlaying());
+
+    dpp::component itemSelectMenu;
+    itemSelectMenu.type = dpp::cot_selectmenu;
+    itemSelectMenu.placeholder = fmt::format("\"{}\": {}", searchResult.query, locale->SearchResult(searchResult.items.size()));
+    itemSelectMenu.custom_id = "";
+    for (int index = 0, size = searchResult.items.size(); index < size && index <= 20; ++index)
+    {
+        const Youtube::Item& item = searchResult.items[index];
+        switch (item.type())
+        {
+            case Youtube::Item::Type::Video:
+            {
+                const Youtube::Video& video = std::get<Youtube::Video>(item);
+                dpp::select_option option;
+                option.value = Signal(Signal::Type::PlayVideo, video.id());
+                option.label = Utility::Truncate(fmt::format("{}: {}", index + 1, video.title()), 100);
+                option.description = Utility::Truncate(locale->SearchOptionDescription(video), 100);
+                itemSelectMenu.add_select_option(option);
+                break;
+            }
+            case Youtube::Item::Type::Playlist:
+            {
+                const Youtube::Playlist& playlist = std::get<Youtube::Playlist>(item);
+                dpp::select_option option;
+                option.value = Signal(Signal::Type::PlayPlaylist, playlist.id());
+                option.label = Utility::Truncate(fmt::format("{}: {}", index + 1, playlist.title()), 100);
+                option.description = Utility::Truncate(locale->SearchOptionDescription(playlist), 100);
+                itemSelectMenu.add_select_option(option);
+                break;
+            }
+        }
+    }
+
+    dpp::component actionRow;
+    actionRow.type = dpp::cot_action_row;
+    actionRow.add_component(itemSelectMenu);
+    return dpp::message().add_embed(embed).add_component(actionRow).set_flags(dpp::m_ephemeral);
+}
+
+dpp::message Bot::Embeds::BadSeek(const Locale::Pointer& locale, const std::string& timestampString)
+{
+    dpp::embed embed;
+    embed.color = Statuses::Problem::Color;
+    embed.description = Statuses::Problem::Prefix + locale->IncorrectTimestamp(timestampString);
+    return dpp::message().add_embed(embed).set_flags(dpp::m_ephemeral);
+}
+
+dpp::message Bot::Embeds::NotPlayingToSeek(const Locale::Pointer& locale)
+{
+    dpp::embed embed;
+    embed.color = Statuses::Problem::Color;
+    embed.description = Statuses::Problem::Prefix + std::string(locale->NotPlayingToSeek());
+    return dpp::message().add_embed(embed).set_flags(dpp::m_ephemeral);
+}
+
+dpp::message Bot::Embeds::SeekingTo(const Locale::Pointer& locale, const std::string& timestamp)
+{
+    dpp::embed embed;
+    embed.color = Statuses::Success::Color;
+    embed.description = Statuses::Success::Prefix + locale->SeekingTo(timestamp);
+    return dpp::message().add_embed(embed);
+}
+
+dpp::message Bot::Embeds::UnknownSlashcommand(const Locale::Pointer& locale)
+{
+    dpp::embed embed;
+    embed.color = Statuses::Problem::Color;
+    embed.description = Statuses::Problem::Prefix + std::string(locale->UnknownSlashcommand());
+    return dpp::message().add_embed(embed).set_flags(dpp::m_ephemeral);
+}
+
+dpp::message Bot::Embeds::UnknownButton(const Locale::Pointer& locale)
+{
+    dpp::embed embed;
+    embed.color = Statuses::Problem::Color;
+    embed.description = Statuses::Problem::Prefix + std::string(locale->UnknownButton());
+    return dpp::message().add_embed(embed).set_flags(dpp::m_ephemeral);
+}
+
+dpp::message Bot::Embeds::UnknownOption(const Locale::Pointer& locale)
+{
+    dpp::embed embed;
+    embed.color = Statuses::Problem::Color;
+    embed.description = Statuses::Problem::Prefix + std::string(locale->UnknownOption());
+    return dpp::message().add_embed(embed).set_flags(dpp::m_ephemeral);
+}
+
+} // namespace kc
