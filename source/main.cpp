2d6f58acc39af737458a113a397c913173661a9f
KontraCity <nikita2006pro@gmail.com>
2024-04-05 18:38:08 +0300

    Initial kc::Youtube::YoutubeError, kc::Youtube::LocalError implementation

--- source/main.cpp
+++ source/main.cpp
@@ -4,8 +4,55 @@
 // Library {fmt}
 #include <fmt/format.h>
 
+// Custom modules
+#include "youtube/error.hpp"
+using namespace kc;
+
 int main()
 {
-    std::cout << fmt::format("Hello, {}!\n", "KontraBot");
-    return 0;
+    try
+    {
+        throw Youtube::YoutubeError(
+            Youtube::YoutubeError::Type::LoginRequired,
+            "s7_qI6_mIXc",
+            "Video unavailable",
+            "This video is private"
+        );
+    }
+    catch (const Youtube::YoutubeError& error)
+    {
+        std::cout << fmt::format(
+            "YouTube error: {}\n"
+            "Type: {}\n"
+            "Item ID: {}\n"
+            "Reason: {}\n"
+            "Subreason: {}\n",
+            error.what(),
+            static_cast<int>(error.type()),
+            error.itemId(),
+            error.reason(),
+            error.subreason()
+        );
+    }
+
+    std::cout << '\n';
+
+    try
+    {
+        throw Youtube::LocalError(
+            Youtube::LocalError::Type::LivestreamNotSupported,
+            "jfKfPfyJRdk"
+        );
+    }
+    catch (const Youtube::LocalError& error)
+    {
+        std::cout << fmt::format(
+            "Local error: {}\n"
+            "Type: {}\n"
+            "Item ID: {}\n",
+            error.what(),
+            static_cast<int>(error.type()),
+            error.itemId()
+        );
+    }
 }
---
+++ source/youtube/error.cpp
@@ -0,0 +1,63 @@
+#include "youtube/error.hpp"
+
+namespace kc {
+
+const char* Youtube::YoutubeError::TypeToName(Type type)
+{
+    switch (type)
+    {
+        case Type::LoginRequired:
+            return "LOGIN_REQUIRED";
+        case Type::Unplayable:
+            return "UNPLAYABLE";
+        case Type::YoutubeError:
+            return "ERROR";
+        case Type::PlaylistError:
+            return "Playlist error";
+        default:
+            return "Unknown error";
+    }
+}
+
+Youtube::YoutubeError::YoutubeError(Type type, const std::string& itemId, const std::string& reason, const std::string& subreason)
+    : m_type(type)
+    , m_itemId(itemId)
+    , m_reason(reason)
+    , m_subreason(subreason)
+{
+    if (subreason.empty())
+        m_what = fmt::format("Item \"{}\": {}: {}", itemId, TypeToName(type), reason);
+    else
+        m_what = fmt::format("Item \"{}\": {}: {}; {}", itemId, TypeToName(type), reason, subreason);
+}
+
+const char* Youtube::LocalError::TypeToName(Type type)
+{
+    switch (type)
+    {
+        case Type::LivestreamNotSupported:
+            return "Livestream is not supported";
+        case Type::PremiereNotSupported:
+            return "Premiere is not supported";
+        case Type::PlaylistNotSupported:
+            return "Playlist is not supported";
+        case Type::PlaylistItemsNotSupported:
+            return "Playlist items are not supported";
+        case Type::EmptyPlaylist:
+            return "Playlist is empty";
+        case Type::AudioNotSupported:
+            return "None of the video's audio tracks are supported";
+        case Type::CouldntDownload:
+            return "Audio URL was found, but download was refused by YouTube";
+        default:
+            return "Unknown error";
+    }
+}
+
+Youtube::LocalError::LocalError(Type type, const std::string& itemId)
+    : m_type(type)
+    , m_itemId(itemId)
+    , m_what(TypeToName(type))
+{}
+
+} // namespace kc
