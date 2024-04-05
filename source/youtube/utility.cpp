bfb3ff6edde1c89d86822be4cc4e308b8c4225d8
KontraCity <nikita2006pro@gmail.com>
2024-04-05 18:44:18 +0300

    Initial kc::Youtube::Utility implementation

---
+++ source/youtube/utility.cpp
@@ -0,0 +1,58 @@
+#include "youtube/utility.hpp"
+
+namespace kc {
+
+std::string Youtube::Utility::ExtractThumbnailUrl(const json& thumbnailsObject)
+{
+    int bestResolution = 0;
+    std::string bestThumbnailUrl;
+    for (const json& thumbnailObject : thumbnailsObject)
+    {
+        int currentResolution = thumbnailObject["width"].get<int>() * thumbnailObject["height"].get<int>();
+        if (currentResolution > bestResolution)
+        {
+            bestResolution = currentResolution;
+            bestThumbnailUrl = thumbnailObject["url"];
+        }
+    }
+    return bestThumbnailUrl;
+}
+
+std::string Youtube::Utility::ExtractString(const json& stringObject)
+{
+    if (stringObject.contains("simpleText"))
+        return stringObject["simpleText"];
+
+    std::string string;
+    for (const json& runObject : stringObject["runs"])
+        string += runObject["text"];
+    return string;
+}
+
+uint64_t Youtube::Utility::ExtractViewCount(std::string viewCountString)
+{
+    /*
+    *   If the item has no views, YouTube API will return "No views" as a view count string instead of "0".
+    *   This has to be checked to ensure that std::stoull() doesn't fail.
+    */
+    if (viewCountString == "No views")
+        return 0;
+
+    viewCountString.erase(
+        std::remove_if(
+            viewCountString.begin(),
+            viewCountString.end(),
+            [](char character) { return (character == ','); }
+        ),
+        viewCountString.end()
+    );
+    return std::stoull(viewCountString);
+}
+
+uint64_t Youtube::Utility::ExtractViewCount(const json& viewCountObject)
+{
+    std::string viewCountString = ExtractString(viewCountObject);
+    return ExtractViewCount(viewCountString);
+}
+
+} // namespace kc
