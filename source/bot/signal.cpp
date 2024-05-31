5d7d1ad4ba117f79398733bebc6c5a818c742bfa
KontraCity <nikita2006pro@gmail.com>
2024-06-01 02:43:39 +0300

    Add deprecated buttons support

--- source/bot/signal.cpp
+++ source/bot/signal.cpp
@@ -12,7 +12,24 @@ Bot::Signal::Signal(const std::string& signalString)
 {
     boost::smatch matches;
     if (!boost::regex_match(signalString, matches, boost::regex(R"((\d+)_([\s\S]*))")))
+    {
+        if (boost::regex_match(signalString, matches, boost::regex(R"(^v?([^"&?\/\s]{11})$)")))
+        {
+            m_data = matches.str(1);
+            m_type = Type::PlayVideo;
+        }
+        else if (boost::regex_match(signalString, matches, boost::regex(R"(^p?(PL[^"&?\/\s]{16,32}$|^OLAK5uy_[^"&?\/\s]{33})$)")))
+        {
+            m_data = matches.str(1);
+            m_type = Type::PlayPlaylist;
+        }
+        else if (boost::regex_match(signalString, matches, boost::regex(R"(^related_([^"&?\/\s]{11})$)")))
+        {
+            m_data = matches.str(1);
+            m_type = Type::RelatedSearch;
+        }
         return;
+    }
     m_data = matches.str(2);
 
     int signalId = std::stoi(matches.str(1));
